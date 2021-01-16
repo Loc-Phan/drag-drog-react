af497ab3d2f258e48df23bc35f892fb218877161
= <pcloc101099@gmail.com>
2021-01-16 14:49:10 +0700

    fix

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+// firebase.utils.js
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB2trjB2tfgk-V8TeUTNnNy1QwyewlUXK4",
+    authDomain: "main-page-b5c0f.firebaseapp.com",
+    databaseURL: "https://main-page-b5c0f.firebaseio.com",
+    projectId: "main-page-b5c0f",
+    storageBucket: "main-page-b5c0f.appspot.com",
+    messagingSenderId: "21146739247",
+    appId: "1:21146739247:web:f7b106f2aebb2c0c501cda",
+    measurementId: "G-6EWWKJG19H"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
